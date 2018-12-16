b94f7f5171907c9b4a064b951a51ba94fb1f21a5
Jason Barmparesos <jason.barmparesos@gmail.com>
2018-12-16 21:40:02 +0200

    Update the style

--- main.c
+++ main.c
@@ -9,8 +9,8 @@ typedef float number_t;
 
 typedef struct
 {
-    number_t* data;
-    int size;
+  number_t *data;
+  int size;
 } Array;
 
 int world_size;
@@ -24,152 +24,146 @@ int world_rank;
  * an integer. On x86 this is done with the BSR instruction.
  */
 static inline int
-log2i(int x)
+log2i (int x)
 {
-    assert(x > 0);
-    return sizeof(int) * 8 - __builtin_clz(x) - 1;
+  assert (x > 0);
+  return sizeof (int) * 8 - __builtin_clz (x) - 1;
 }
 
 void
-test_log2i()
+test_log2i ()
 {
-    int n = 1;
-    for (int i = 0; i < 15; i++) {
-        assert(log2i(n) == i);
-        n *= 2;
+  int n = 1;
+  for (int i = 0; i < 15; i++)
+    {
+      assert (log2i (n) == i);
+      n *= 2;
     }
 }
 
-float
-masterPart(int world_size,
-           int world_rank,
-           int size,
-           int partLength,
-           float* numberPart,
-           MPI_Comm comm);
+float masterPart (int world_size, int world_rank, int size, int partLength,
+		  float *numberPart, MPI_Comm comm);
 
-void
-slavePart(int world_rank,
-          int partLength,
-          float* numberPart,
-          int size,
-          MPI_Comm comm);
+void slavePart (int world_rank, int partLength, float *numberPart, int size,
+		MPI_Comm comm);
 
-Array*
-array_new_random(int size)
+Array *
+array_new_random (int size)
 {
-    Array* array = (Array*) malloc(sizeof(*array));
-    array->size = size;
-    array->data = (number_t*) malloc(sizeof(number_t) * size);
-    int cal = 5;
-    srand((cal + 1) * time(NULL));
+  Array *array = (Array *) malloc (sizeof (*array));
+  array->size = size;
+  array->data = (number_t *) malloc (sizeof (number_t) * size);
+  int cal = 5;
+  srand ((cal + 1) * time (NULL));
 
-    for (int i = 0; i < size; i++)
-        array->data[i]
-          = world_rank * size + i; // (number_t)(rand() - rand()) * 0.05f;
-    return array;
+  for (int i = 0; i < size; i++)
+    array->data[i]
+      = world_rank * size + i; // (number_t)(rand() - rand()) * 0.05f;
+  return array;
 }
 
 void
-array_free(Array* array)
+array_free (Array *array)
 {
-    free(array->data);
-    free(array);
+  free (array->data);
+  free (array);
 }
 
 int
-group_size(int l)
+group_size (int l)
 {
-    return world_size / (1 << l);
+  return world_size / (1 << l);
 }
 
 int
-group_number(int l)
+group_number (int l)
 {
-    return world_rank / group_size(l);
+  return world_rank / group_size (l);
 }
 
-Array*
-array_distances_from_vp(Array* dataset, number_t vantage_point)
+Array *
+array_distances_from_vp (Array *dataset, number_t vantage_point)
 {
-    Array* distances = (Array*) malloc(sizeof(Array));
-    distances->size = dataset->size;
-    distances->data = (number_t*) malloc(sizeof(number_t) * distances->size);
-    for (int i = 0; i < dataset->size; i++) {
-        distances->data[i] = fabs(
-          vantage_point - dataset->data[i]); // we calculate the distance of the
-                                             // ith point from the vantage point
+  Array *distances = (Array *) malloc (sizeof (Array));
+  distances->size = dataset->size;
+  distances->data = (number_t *) malloc (sizeof (number_t) * distances->size);
+  for (int i = 0; i < dataset->size; i++)
+    {
+      distances->data[i] = fabs (
+	vantage_point - dataset->data[i]); // we calculate the distance of the
+					   // ith point from the vantage point
     }
-    return distances;
+  return distances;
 }
 
 void
-test_array_distances_from_vp()
+test_array_distances_from_vp ()
 {
-    Array* dataset = array_new_random(5);
-    number_t vp = 4.56f;
-    Array* distances = array_distances_from_vp(dataset, vp);
-    for (int i = 0; i < dataset->size; i++) {
-        printf(
-          "PROCESS %d: vantage_point = %f, point[%d] = %f and distance = %f\n",
-          world_rank, vp, i, dataset->data[i], distances->data[i]);
+  Array *dataset = array_new_random (5);
+  number_t vp = 4.56f;
+  Array *distances = array_distances_from_vp (dataset, vp);
+  for (int i = 0; i < dataset->size; i++)
+    {
+      printf (
+	"PROCESS %d: vantage_point = %f, point[%d] = %f and distance = %f\n",
+	world_rank, vp, i, dataset->data[i], distances->data[i]);
     }
-    array_free(dataset);
-    /** @todo Do some actual testing with assert() */
+  array_free (dataset);
+  /** @todo Do some actual testing with assert() */
 }
 
 int
-less_than_median(Array* array, number_t vantage_point)
+less_than_median (Array *array, number_t vantage_point)
 {
-    int count_points
-      = 0; // counts how many points are closer to vantage point than median.
-    return count_points;
+  int count_points
+    = 0; // counts how many points are closer to vantage point than median.
+  return count_points;
 }
 
 int
-master_rank(int l)
+master_rank (int l)
 {
-    return world_rank - (world_rank % group_size(l));
+  return world_rank - (world_rank % group_size (l));
 }
 
 void
-test_partitioning()
+test_partitioning ()
 {
-    world_size = 16;
-    world_rank = 12;
-    assert(master_rank(0) == 0);
-    assert(master_rank(1) == 8);
-    assert(master_rank(2) == 12);
+  world_size = 16;
+  world_rank = 12;
+  assert (master_rank (0) == 0);
+  assert (master_rank (1) == 8);
+  assert (master_rank (2) == 12);
 }
 
 MPI_Comm
-communicator_for_level(int l)
+communicator_for_level (int l)
 {
-    MPI_Comm c;
-    MPI_Comm_split(MPI_COMM_WORLD, group_number(l), 0, &c);
-    return c;
+  MPI_Comm c;
+  MPI_Comm_split (MPI_COMM_WORLD, group_number (l), 0, &c);
+  return c;
 }
 
 void
-split_parallel(int l)
+split_parallel (int l)
 {
-    MPI_Comm comm = communicator_for_level(l);
-    MPI_Comm_free(&comm);
+  MPI_Comm comm = communicator_for_level (l);
+  MPI_Comm_free (&comm);
 }
 
 int
-main(int argc, char** argv)
+main (int argc, char **argv)
 {
-    test_partitioning();
-    test_log2i();
+  test_partitioning ();
+  test_log2i ();
 
-    MPI_Init(&argc, &argv);
+  MPI_Init (&argc, &argv);
 
-    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+  MPI_Comm_rank (MPI_COMM_WORLD, &world_rank);
+  MPI_Comm_size (MPI_COMM_WORLD, &world_size);
 
-    Array* dataset = array_new_random(5);
-    int dataset_size = dataset->size * world_size;
+  Array *dataset = array_new_random (5);
+  int dataset_size = dataset->size * world_size;
 
 #if 0
     for (int l = 0; group_size(l) > 1; l++) {
@@ -177,16 +171,19 @@ main(int argc, char** argv)
     }
 #endif
 
-    if (world_rank == 0) {
-        float median = masterPart(world_size, world_rank, dataset_size,
-                                  dataset->size, dataset->data, MPI_COMM_WORLD);
-        printf("Median: %.2f\n", median);
-    } else {
-        slavePart(world_rank, dataset->size, dataset->data, dataset_size,
-                  MPI_COMM_WORLD);
+  if (world_rank == 0)
+    {
+      float median = masterPart (world_size, world_rank, dataset_size,
+				 dataset->size, dataset->data, MPI_COMM_WORLD);
+      printf ("Median: %.2f\n", median);
+    }
+  else
+    {
+      slavePart (world_rank, dataset->size, dataset->data, dataset_size,
+		 MPI_COMM_WORLD);
     }
 
-    array_free(dataset);
-    MPI_Finalize();
-    return 0;
+  array_free (dataset);
+  MPI_Finalize ();
+  return 0;
 }
